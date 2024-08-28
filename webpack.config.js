eecba1a4472a2e01dc27275b229be0b4288d69df
richard <theirsaviorhd@gmail.com>
2024-08-28 17:00:50 -0400

    init html webpack plugin for output management

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -17,5 +18,15 @@ module.exports = {
         type: "asset/resource",
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
