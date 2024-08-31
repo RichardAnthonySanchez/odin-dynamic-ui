61803ed4d0f5df94a86ec1e58c6a75c1a0245e7d
richard <theirsaviorhd@gmail.com>
2024-08-31 05:40:43 -0400

    added a event listener test for clicking an element that will be later used to display a menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import "./style.css";
 
-function component() {}
+function component() {
+  document.addEventListener("click", function (e) {
+    if (e.target.matches(".menu-trigger")) {
+      console.log("you clicked the trigger");
+    }
+  });
+}
 
 document.body.appendChild(component());
