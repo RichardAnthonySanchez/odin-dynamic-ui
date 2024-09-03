77b123a2f013449d3588541c9b86157ebea89a5e
richard <theirsaviorhd@gmail.com>
2024-09-03 17:14:52 -0400

    added a method that allows users to navigate forward and backward  through arrays

---
+++ src/modelPagination.js
@@ -0,0 +1,17 @@
+function modelPagination(state, pages, input) {
+  if (input === "right") {
+    if (state++ > pages.length + 1) {
+      state = pages[0];
+    } else {
+      state = state++;
+    }
+  } else {
+    if (state-- < pages[0]) {
+      state = pages.length + 1;
+    } else {
+      state = state--;
+    }
+  }
+}
+
+export default modelPagination;
