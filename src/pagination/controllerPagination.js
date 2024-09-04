cbacd53ca151dd64149121a323bf42abf0151e16
richard <theirsaviorhd@gmail.com>
2024-09-04 17:46:32 -0400

    event listeners now how correct selectors for each element and we console log the state as it updates

--- src/pagination/controllerPagination.js
+++ src/pagination/controllerPagination.js
@@ -2,11 +2,12 @@ import modelPagination from "./modelPagination";
 
 function controllerPagination(state, pages) {
   document.addEventListener("click", function (e) {
-    if (e.target.matches(".right")) {
+    if (e.target.matches(".carousel-right")) {
       state = modelPagination(state, pages, "right");
-    } else {
+    } else if (e.target.matches(".carousel-left")) {
       state = modelPagination(state, pages, "left");
     }
+    console.log("state has been updated to " + state);
     return state;
   });
 }
