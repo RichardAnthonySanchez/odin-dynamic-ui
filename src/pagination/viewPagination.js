0f622d3850cfffb494ca661fd8c8336185319878
richard <theirsaviorhd@gmail.com>
2024-09-05 17:20:28 -0400

    added image content to our containers

--- src/pagination/viewPagination.js
+++ src/pagination/viewPagination.js
@@ -1,12 +1,27 @@
 import controllerPagination from "./controllerPagination";
 
+import Homepage from "../img/img-todoslist-homepage.png";
+import ProjectManager from "../img/img-todoslist-projectmanager.png";
+import TaskManager from "../img/img-todoslist-taskmanager.png";
+
 function viewPagination(state) {
   let pages = [0, 1, 2];
-  const slidesContent = document.querySelector(
-    ".slides-container span:first-child"
-  );
 
-  slidesContent.innerHTML = state;
+  const positionOneContainer = document.querySelector(".position-one");
+  const positionTwoContainer = document.querySelector(".position-two");
+  const positionThreeContainer = document.querySelector(".position-three");
+
+  const homepageImage = new Image();
+  const projectManagerImage = new Image();
+  const taskManagerImage = new Image();
+
+  homepageImage.src = Homepage;
+  projectManagerImage.src = ProjectManager;
+  taskManagerImage.src = TaskManager;
+
+  positionOneContainer.appendChild(homepageImage);
+  positionTwoContainer.appendChild(projectManagerImage);
+  positionThreeContainer.appendChild(taskManagerImage);
 
   controllerPagination(state, pages);
 }
