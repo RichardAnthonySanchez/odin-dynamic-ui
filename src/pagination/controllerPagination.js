871aa30a94dc31af89864011b22b7caec19ab54f
richard <theirsaviorhd@gmail.com>
2024-09-08 05:24:49 -0400

    the user can now click the radio buttons to update the state of the image carousel

--- src/pagination/controllerPagination.js
+++ src/pagination/controllerPagination.js
@@ -7,6 +7,12 @@ function controllerPagination(state, pages) {
       state = modelPagination(state, pages, "right");
     } else if (e.target.matches(".carousel-left")) {
       state = modelPagination(state, pages, "left");
+    } else if (e.target.id === "nav1") {
+      state = 0;
+    } else if (e.target.id === "nav2") {
+      state = 1;
+    } else if (e.target.id === "nav3") {
+      state = 2;
     }
     console.log("state has been updated to " + state);
     viewPagination(state);
